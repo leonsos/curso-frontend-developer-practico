fc3441a02843827a303a7470607eda79e3166e02
leon sosa <leonsosa3@gmail.com>
2023-03-06 20:00:47 -0400

    menu fusion

---
+++ app.js
@@ -0,0 +1,7 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click',toggleMenuDesktop);
+function toggleMenuDesktop () {
+    desktopMenu.classList.toggle('inactive');
+}
